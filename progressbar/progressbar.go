d464e2d3efd5e2920ef1923e219b76cec6532419
moritz <moritz@zendri.com>
2021-03-09 15:35:29 +0100

    Enhance progress bar

--- progressbar/progressbar.go
+++ progressbar/progressbar.go
@@ -1,27 +1,35 @@
 package progressbar
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type Bar struct {
-	cur   int    // current progress
 	total int    // total value for progress
 	per   int    // current in percentage
 	bar   string // the actual progress bar to be printed
 }
 
 func New(total int) *Bar {
-	return &Bar{total: total}
+	return &Bar{
+		total: total,
+		bar:   strings.Repeat("-", 100),
+	}
 }
 
 func (b *Bar) Update(newCur int) {
-	newPer := newCur * 100 / b.total
+	newPer := (newCur * 100 / b.total) + 1
 	if newPer == b.per {
 		return
 	}
 
-	b.cur = newCur
 	b.per = newPer
-	b.bar += "#"
+	b.bar = strings.Replace(b.bar, "-", "▊", 1)
 
-	fmt.Printf("\r%v", b.bar)
+	fmt.Printf("\r[%v/100] %v", b.per, b.bar)
+
+	if b.per == 100 {
+		fmt.Println("\nDone!")
+	}
 }
