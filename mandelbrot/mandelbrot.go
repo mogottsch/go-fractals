44443b84e70c0629a26cc38dd3080d01fd300fc0
moritz <moritz@zendri.com>
2021-03-09 13:13:07 +0100

    Add translate

--- mandelbrot/mandelbrot.go
+++ mandelbrot/mandelbrot.go
@@ -3,30 +3,52 @@ package main
 import (
 	"fmt"
 	"image"
+	"image/color"
 	"image/png"
 	"os"
 	"strconv"
 )
 
-var img image.Image
+var img *image.RGBA
+var width int
+var height int
 
 func main() {
-	initImage()
-	saveImage()
+	initImg()
+	save()
 }
 
-func initImage() {
+func initImg() {
 	args := os.Args[1:]
 	fmt.Println(args)
 
-	width, _ := strconv.Atoi(args[0])
-	height, _ := strconv.Atoi(args[1])
+	width, _ = strconv.Atoi(args[0])
+	height, _ = strconv.Atoi(args[1])
 
 	rect := image.Rect(0, 0, width, height)
 	img = image.NewRGBA(rect)
+
+	setPixels(func(x, y int) color.Color {
+		return color.RGBA{0, 0, 0, 255}
+	})
 }
 
-func saveImage() {
+func setPixels(fn func(x, y int) color.Color) {
+	for y := 0; y < height; y++ {
+		for x := 0; x < height; x++ {
+			img.Set(x, y, fn(x, y))
+		}
+	}
+}
+
+func translate(x, y int) complex128 {
+	return complex(
+		float64(x)/float64(width)*2-1,
+		float64(y)/float64(height)*2-1,
+	)
+}
+
+func save() {
 	file, err := os.Create("mandelbrot.png")
 	if err != nil {
 		panic(err)
